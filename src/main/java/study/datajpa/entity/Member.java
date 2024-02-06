e1c460221dbaaaaf0485a581fe0011ae0ee8d5cf
jin-su-jin <sujinbest@gmail.com>
2024-02-06 12:56:28 +0900

    소스커밋

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,41 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id" , "username" , "age"})
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+    private int age;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username, int i, Team team) {
+        this.username = username;
+        this.age = i;
+        if(team != null){
+            this.changeTeam(team);
+        }
+
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+    public Member(String username){
+        this.username = username;
+    }
+}
